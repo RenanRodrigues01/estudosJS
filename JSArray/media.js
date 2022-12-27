a76920b376800d8596ddfd8ea737af5007670111
renan <renan.rodriguesmoura2022@gmail.com>
2022-12-27 16:55:46 -0300

    refatorando media com o uso do for of

--- JSArray/media.js
+++ JSArray/media.js
@@ -1,11 +1,21 @@
 // declarando um array
 const notas =[10, 6.5, 8, 7.5];
-// adicona um item a última posição
+/* adicona um item a última posição
 notas.push(5)
-// remove o último elemnto do array
+ remove o último elemnto do array
 notas.pop()
-                                                        // length sempre traz o tamanho do array, neste caso 4
+                                                         length sempre traz o tamanho do array, neste caso 4
 const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
 console.log(media);
-console.log(notas);
+console.log(notas);*/
+//calculando média com o uso do for of
+let somaDasNotas = 0;
+
+for (let nota of notas){
+    somaDasNotas += nota
+}
+
+const media = somaDasNotas / notas.length;
+
+console.log(`A media final é ${media}`)
