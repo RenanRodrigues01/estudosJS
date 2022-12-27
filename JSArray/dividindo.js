642615ed6280459caff002408d7209051189c2e7
renan <renan.rodriguesmoura2022@gmail.com>
2022-12-27 14:24:02 -0300

    melhorando metodo slice()

--- JSArray/dividindo.js
+++ JSArray/dividindo.js
@@ -23,9 +23,9 @@ const alunos = [
 
 //slice= fatiamento, primeiro parametro é onde começa a divisão e o segundo quantos itens vão divididos, neste caso vai do indice 0 ao 9
 //metodo não altera o array original, ele gera um novo 
-const sala1 = alunos.slice(0, 10)
+const sala1 = alunos.slice(0, alunos.length / 2)
 // não precisa de um segundo parametro pois no caso vamos pegar do indice 10 até o fim do array
-const sala2 = alunos.slice(10)
+const sala2 = alunos.slice(alunos.length / 2)
 
 console.log(sala1);
 console.log(sala2);
