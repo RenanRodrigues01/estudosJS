fd9e5c50cc02bdce81799563caf3123fa5ab1e53
renan <renan.rodriguesmoura2022@gmail.com>
2022-12-26 23:53:34 -0300

    adicionando um item ao array

--- JSArray/media.js
+++ JSArray/media.js
@@ -1,5 +1,9 @@
+// declarando um array
 const notas =[10, 6.5, 8, 7.5];
-
+// adicona um item a última posição
+notas.push(5)
+                                                        // length sempre traz o tamanho do array, neste caso 4
 const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
 
 console.log(media);
+console.log(notas);
