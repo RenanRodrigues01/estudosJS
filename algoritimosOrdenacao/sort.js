8f8ca5d6f442a3a9d48a0c05c77a1e2dd49cdd19
renan <renan.rodriguesmoura2022@gmail.com>
2023-01-05 15:26:48 -0300

    criando a mesma solução com metodos JavaScript

--- algoritimosOrdenacao/sort.js
+++ algoritimosOrdenacao/sort.js
@@ -5,16 +5,35 @@ for(let atual = 0; atual < livros.length; atual++){
     let menor = menorValor(livros, atual);
 
     console.log('posição atual', atual);
-
+//verica qual é o item atual que está sendo percorrido
     let livroAtual = livros[atual];
     console.log('livro atual', livros[atual]);
-
+//guarda qual é o item que tem o menor valor 
     let livroMenorPreço = livros[menor];
     console.log('livro menor preço', livros[menor], 'indice', menor);
-
+//troca as posições do array, o inidice atual recebe o livro de menor valor, e o indice onde o menor livro estava recebe o livro atual 
     livros[atual] = livroMenorPreço;
     livros[menor] = livroAtual;
 }
 
 console.log(livros);
+/*      ---------------------||--------------------||-------------------------||-------------------------
+exeuta a mesma função criada a cima, mas este é um metodo pronto, que o proprio JS oferece
+porem segue a mesma logica criada nas duas funções utilizadas 
+*/
+function ordenaValor(lista, chave){
+    const listaOrdenada = lista.sort( (a, b) => {
+// metodo percorre o o array comparando os elementos e ordenando do menor para o maior 
+        if(a[chave] < b[chave] ){
+            return -1
+        }
+        if(a[chave] > b[chave]){
+            return 1
+        }
+        return 0
+    });
+    return listaOrdenada;
+}
 
+const metodoSort = ordenaValor(livros, "valor");
+console.log(metodoSort);
