bea562feeaf4b16baacd53f51a38c6c455550499
renan <renan.rodriguesmoura2022@gmail.com>
2023-01-24 11:19:57 -0300

    atalho nas declaraçoes de tipo

--- typescript-curso-1-arquivos-iniciais/app/models/negociacoes.ts
+++ typescript-curso-1-arquivos-iniciais/app/models/negociacoes.ts
@@ -1,12 +1,15 @@
 import { Negociacao } from "./negociacao.js";
 
+// sintaxe opicional para declaração de tipo
+// private negociacoes: Negociacao[] = [];
 export class Negociacoes {
     private negociacoes: Array<Negociacao> = [];
 
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao);
     }
-
+// sintaxe alternativa para declaração de apenas leitura
+//  listar(): readonly negociacao[]
     listar(): ReadonlyArray<Negociacao> {
         return this.negociacoes;
     }
