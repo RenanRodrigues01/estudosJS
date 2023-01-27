8761af767e193a28b5424c191967bc10225c214a
renan <renan.rodriguesmoura2022@gmail.com>
2023-01-27 16:05:36 -0300

    criando modelo de view para negociacoes

---
+++ typescript-curso-1-arquivos-iniciais/app/views/negociacoes-view.ts
@@ -0,0 +1,18 @@
+export class negocacoesView {
+    template() : string {
+        return `
+        <table class="table table-hover table-bordered">
+            <thead>
+                <tr>
+                    <th>DATA</th>
+                    <th>Quantidade</th>
+                    <th>VOLUME</th>
+                </tr>
+            </thead>
+            <tbody>
+            
+            </tbody>
+        </table>
+        `;
+    }
+}
