37f866799546f64fc518d14a3901d4936f444a3c
renan <renan.rodriguesmoura2022@gmail.com>
2023-01-28 14:50:33 -0300

    implementando metodo abstrato a classe view

--- typescript-curso-1-arquivos-iniciais/app/views/view.ts
+++ typescript-curso-1-arquivos-iniciais/app/views/view.ts
@@ -1,16 +1,18 @@
 /*Crinado uma classe usando o tipo generics, para extnder seus metodos as filhas*/
-export class View<T> {
+export abstract class View<T> {
+/*criamos uma classe abstrata, para previnir a crição de uma instancia de view, já que a view está incompleta
+pois sem a filha implementar o metodo template, view só iria exibir um erro em run time  */
+
     protected elemento: HTMLElement;
 
     constructor(seletor: string) {
         this.elemento = document.querySelector(seletor)
     }
 
-    
-    template(model: T): string {
-       throw Error("A classe filha precisa implementar o metodo template")
- 
-    }
+/* criando um metodo abstrato, classe não define como o metodo será implementado mas sim a filha,
+sendo assim o metodo torna obrigatorio, e detectavel em tempo de desenvolvimento */
+    abstract template(model: T): string;
+
 
     update(model : T) : void {
         const template = this.template(model);
--- typescript-curso-1-arquivos-iniciais/dist/js/views/view.js
+++ typescript-curso-1-arquivos-iniciais/dist/js/views/view.js
@@ -3,9 +3,6 @@ export class View {
     constructor(seletor) {
         this.elemento = document.querySelector(seletor);
     }
-    template(model) {
-        throw Error("A classe filha precisa implementar o metodo template");
-    }
     update(model) {
         const template = this.template(model);
         this.elemento.innerHTML = template;
