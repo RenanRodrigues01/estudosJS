eecf178527557cfa7dd268b18c9d3f357ea15136
renan <renan.rodriguesmoura2022@gmail.com>
2023-01-28 17:27:50 -0300

    melhorando a declaração das enums com boas praticas

--- typescript-curso-1-arquivos-iniciais/app/enums/dias-da-semana.ts
+++ typescript-curso-1-arquivos-iniciais/app/enums/dias-da-semana.ts
@@ -1,10 +1,11 @@
-
+/*por padrão o TS enumera as enums começando do 0, mas mesmo que os valores que vc vá usar saõ uma sequencia
+é uma boa pratica sempre delarar os valores das enums, elas tambem podem ser strings */
 export enum DiasDaSemana {
-    DOMINGO,
-    SEGUNDA,
-    TERCA,
-    QUARTA,
-    QUINTA,
-    SEXTA,
-    SABADO
+    DOMINGO = 0,
+    SEGUNDA = 1,
+    TERCA = 2,
+    QUARTA = 3,
+    QUINTA = 4,
+    SEXTA = 5,
+    SABADO = 6
 }
